99cde3b2f0b0a2808a5060c82d801c431a151f2c
jjreina <jreinapascual@gmail.com>
2023-05-09 11:56:46 +0200

    Composition

--- 02_Fernando_Herrera/01-typescript-intro/src/topics/08-classes.ts
+++ 02_Fernando_Herrera/01-typescript-intro/src/topics/08-classes.ts
@@ -8,11 +8,24 @@
   }
 }
 
+/*
 export class Hero extends Person {
-  constructor( public alterEgo: string, public age: number, public realName: string) {
-    super(realName, 'New York');
-  }
+constructor( public alterEgo: string, public age: number, public realName: string) {
+  super(realName, 'New York');
+}
+}
+*/
+
+// Composition
+export class Hero {
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string,
+    public person: Person
+  ) {}
 }
 
-const ironman = new Hero('Ironman', 45, 'Tony');
+const tony: Person = new Person("Tony Stark", "New York");
+const ironman = new Hero("Ironman", 45, "Tony", tony);
 console.log(ironman);
