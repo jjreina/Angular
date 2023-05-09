e87bf4f4df685954ae89dd74cd224fb154d501f8
jjreina <jreinapascual@gmail.com>
2023-05-09 12:50:14 +0200

    Optional Chaining

---
+++ 02_Fernando_Herrera/01-typescript-intro/src/topics/11-optional-chaining.ts
@@ -0,0 +1,23 @@
+﻿export interface Passenger {
+  name: string;
+  children?: string[];
+}
+
+const passenger1: Passenger = {
+  name: "Juan",
+};
+
+const passenger2: Passenger = {
+  name: "Bea",
+  children: ["Ale", "Dani"],
+};
+
+const returnChildrenNumber = ({ name, children }: Passenger): number => {
+  const howManyChildren = children?.length || 0;
+  // const howManyChildren = children!.length;
+  console.log(name, howManyChildren);
+  return howManyChildren;
+};
+
+returnChildrenNumber(passenger1);
+returnChildrenNumber(passenger2);
