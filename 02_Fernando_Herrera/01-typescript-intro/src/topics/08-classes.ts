ed2314a745c0c8cd0765cae18e8568a82aee3919
jjreina <jreinapascual@gmail.com>
2023-05-09 09:32:25 +0200

    Classes

---
+++ 02_Fernando_Herrera/01-typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,18 @@
+﻿export class Person {
+  private name: string;
+  private address: string;
+
+  constructor(name: string, address: string) {
+    this.name = name;
+    this.address = address;
+  }
+}
+
+export class Hero extends Person {
+  constructor( public alterEgo: string, public age: number, public realName: string) {
+    super(realName, 'New York');
+  }
+}
+
+const ironman = new Hero('Ironman', 45, 'Tony');
+console.log(ironman);
