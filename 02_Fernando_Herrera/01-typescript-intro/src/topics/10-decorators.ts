c536b8e989656879ad6d8c1544fe84a94937b97d
jjreina <jreinapascual@gmail.com>
2023-05-09 12:22:58 +0200

    decorators

---
+++ 02_Fernando_Herrera/01-typescript-intro/src/topics/10-decorators.ts
@@ -0,0 +1,22 @@
+﻿function classDecorator<T extends { new (...args: any[]): {} }>(
+  constructor: T
+) {
+  return class extends constructor {
+    newProperty: string = "New Property";
+    hello: string = "overrride";
+  };
+}
+
+@classDecorator
+export class SuperClass {
+  public muProperty: string = "Abc123";
+
+  print() {
+    console.log("Hello World");
+  }
+}
+
+console.log(SuperClass);
+
+const myClass = new SuperClass();
+console.log(myClass);
