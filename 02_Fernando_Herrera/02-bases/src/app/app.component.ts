11fed01dcc5af2cc5bebefac7e60693325e793c0
jjreina <jreinapascual@gmail.com>
2023-05-10 10:00:33 +0200

    Basic Counter

--- 02_Fernando_Herrera/02-bases/src/app/app.component.ts
+++ 02_Fernando_Herrera/02-bases/src/app/app.component.ts
@@ -5,7 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   public title: string = 'My first app';
   public counter: number = 10;
+
+  increaseCounter = (): void => { this.counter++ }
+
+  decreaseCounter = (): void => { this.counter-- }
+
+  resetCounter = (): void => { this.counter = 0 }
+
+  behaviorCounter = (value: number): void => {
+    this.counter += value
+  }
+
+
+
 }
