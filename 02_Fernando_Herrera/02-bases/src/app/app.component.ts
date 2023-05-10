863f223dfde41be112a524beb3773d030a537325
jjreina <jreinapascual@gmail.com>
2023-05-10 10:20:47 +0200

    Added functionality to the counter component

--- 02_Fernando_Herrera/02-bases/src/app/app.component.ts
+++ 02_Fernando_Herrera/02-bases/src/app/app.component.ts
@@ -8,18 +8,4 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   public title: string = 'My first app';
-  public counter: number = 10;
-
-  increaseCounter = (): void => { this.counter++ }
-
-  decreaseCounter = (): void => { this.counter-- }
-
-  resetCounter = (): void => { this.counter = 0 }
-
-  behaviorCounter = (value: number): void => {
-    this.counter += value
-  }
-
-
-
 }
--- 02_Fernando_Herrera/02-bases/src/app/counter/counter.component.ts
+++ 02_Fernando_Herrera/02-bases/src/app/counter/counter.component.ts
@@ -2,8 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hello Counter</h1>'
+  template: `
+    <h3>Counter: {{ counter }}</h3>
+    <button (click)="increaseCounter()">Add</button>
+    <button (click)="decreaseCounter()">Subtract</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="behaviorCounter(1)">+1</button>
+    <button (click)="behaviorCounter(-1)">-1</button>
+  `
 })
-export class CounterComponent {
 
+export class CounterComponent {
+  protected counter: number = 10
+  constructor() { }
+
+  increaseCounter = (): void => { this.counter++ }
+
+  decreaseCounter = (): void => { this.counter-- }
+
+  resetCounter = (): void => { this.counter = 0 }
+
+  behaviorCounter = (value: number): void => {
+    this.counter += value
+  }
 }
