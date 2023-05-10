b1963fab0ba8ad4112f103d56d2b6d8f4c862679
jjreina <jreinapascual@gmail.com>
2023-05-10 18:04:01 +0200

    Created new components via angular cli ng g c

--- 02_Fernando_Herrera/02-bases/src/app/app.module.ts
+++ 02_Fernando_Herrera/02-bases/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
