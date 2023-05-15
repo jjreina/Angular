863d810af6b4830affb4ad64b1dcfa0084fbc622
jjreina <jreinapascual@gmail.com>
2023-05-15 11:03:31 +0200

    ng-template directive

--- 02_Fernando_Herrera/02-bases/src/app/heroes/list/list.component.ts
+++ 02_Fernando_Herrera/02-bases/src/app/heroes/list/list.component.ts
@@ -13,4 +13,9 @@ export class ListComponent {
     'She Hulk',
     'Thor',
   ];
+  public deletedHero: string | undefined;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
