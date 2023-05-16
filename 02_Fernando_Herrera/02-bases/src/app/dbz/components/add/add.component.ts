de06de67820f9a6998c206b1b172baf88f0aa9b9
jjreina <jreinapascual@gmail.com>
2023-05-16 19:12:59 +0200

    Refactor: created two new components in dbz modile

---
+++ 02_Fernando_Herrera/02-bases/src/app/dbz/components/add/add.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-characters',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css'],
+})
+export class AddComponent {}
--- 02_Fernando_Herrera/02-bases/src/app/dbz/dbz.module.ts
+++ 02_Fernando_Herrera/02-bases/src/app/dbz/dbz.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddComponent } from './components/add/add.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent, AddComponent],
   exports:[MainPageComponent],
   imports: [
     CommonModule
