528f7eb754c6161ddaf4b6f5ca7587aabeb6bb05
jjreina <jreinapascual@gmail.com>
2023-05-17 19:04:44 +0200

    ngModel and FormsModule

--- 02_Fernando_Herrera/02-bases/src/app/dbz/components/add/add.component.ts
+++ 02_Fernando_Herrera/02-bases/src/app/dbz/components/add/add.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-characters',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css'],
 })
-export class AddComponent {}
+export class AddComponent {
+  public character: Character = {
+    name: '',
+    power: 0,
+  };
+
+  public emitCharacter(): void {
+    console.log(this.character);
+  }
+}
