39b00dea19ebd193e7bfc52cb7d9b75abf10d15e
jjreina <jreinapascual@gmail.com>
2023-05-18 19:51:11 +0200

    ako - 01 - Items from arrays and delete item

--- 01_Akotech/01-shipping-cart/src/app/shopping-cart/components/item/shopping-cart-item.component.ts
+++ 01_Akotech/01-shipping-cart/src/app/shopping-cart/components/item/shopping-cart-item.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartItem } from '../../interfaces/cart-item';
 
 @Component({
   selector: 'app-shopping-cart-item',
   templateUrl: './shopping-cart-item.component.html',
-  styleUrls: ['./shopping-cart-item.component.css']
+  styleUrls: ['./shopping-cart-item.component.css'],
 })
 export class ShoppingCartItemComponent {
+  @Input('cartItem')
+  cartItem: CartItem = {
+    imageUrl: '',
+    name: '',
+    price: 0,
+  };
 
+  @Output()
+  cartItemDelete = new EventEmitter<void>();
+
+  public onDeleteClicked(): void {
+    this.cartItemDelete.emit();
+  }
 }
--- 01_Akotech/01-shipping-cart/src/app/shopping-cart/components/shopping-cart.component.ts
+++ 01_Akotech/01-shipping-cart/src/app/shopping-cart/components/shopping-cart.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { CartItem } from '../interfaces/cart-item';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
 })
 export class ShoppingCartComponent {
+  cartItems: CartItem[] = [
+    {
+      imageUrl: 'headphones.jpg',
+      name: 'Headphones',
+      price: 50,
+    },
+    {
+      imageUrl: 'keyboard.jpg',
+      name: 'Keyboard',
+      price: 14.5,
+    },
+    {
+      imageUrl: 'monitor.jpg',
+      name: 'Monitor',
+      price: 199.99,
+    },
+  ];
 
+  public deleteItem(itemToDelete: CartItem): void {
+    console.log(itemToDelete.name);
+    this.cartItems = this.cartItems.filter((item) => item !== itemToDelete);
+  }
+
+  public total(): number {
+    // return this.cartItems.reduce((acc, item) => (acc = +item.price), 0);
+    // Destructuring
+    return this.cartItems.reduce((acc, { price }) => (acc = +price), 0);
+  }
 }
