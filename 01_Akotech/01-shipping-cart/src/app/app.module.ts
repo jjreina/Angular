663a6fc420b7967cefb639e541cc1f79fd603524
jjreina <jreinapascual@gmail.com>
2023-05-18 12:42:59 +0200

    Minor structure refactor

--- 01_Akotech/01-shipping-cart/src/app/app.module.ts
+++ 01_Akotech/01-shipping-cart/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { ShoppingCartItemComponent } from './shopping-cart/shopping-cart-item.component';
+import { ShoppingCartComponent } from './shopping-cart/components/shopping-cart.component';
+import { ShoppingCartItemComponent } from './shopping-cart/components/item/shopping-cart-item.component';
 
 @NgModule({
   declarations: [
