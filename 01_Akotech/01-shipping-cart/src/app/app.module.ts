202c1cfcaf59c36c8527a9b54a66578031a93c58
jjreina <jreinapascual@gmail.com>
2023-05-23 20:55:44 +0200

    New header components

--- 01_Akotech/01-shipping-cart/src/app/app.module.ts
+++ 01_Akotech/01-shipping-cart/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/components/shopping-cart.component';
 import { ShoppingCartItemComponent } from './shopping-cart/components/item/shopping-cart-item.component';
+import { HeaderShoppingCartComponent } from './shopping-cart/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
     ShoppingCartItemComponent,
+    HeaderShoppingCartComponent,
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ 01_Akotech/01-shipping-cart/src/app/shopping-cart/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopping-cart-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderShoppingCartComponent {}
